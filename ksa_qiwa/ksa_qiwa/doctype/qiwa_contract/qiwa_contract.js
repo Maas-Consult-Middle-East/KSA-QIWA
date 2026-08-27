6880ff22ca454e8bba4f531f77c6982d2e2b547a
Mac Avenue <salmana@enfono.com>
2026-08-27 17:44:06 +0300

    fix:qiwa contract doctype deleted

--- ksa_qiwa/ksa_qiwa/doctype/qiwa_contract/qiwa_contract.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2026, Salman Adayatt and contributors
-// For license information, please see license.txt
-
-// frappe.ui.form.on("QIWA Contract", {
-// 	refresh(frm) {
-
-// 	},
-// });
--- ksa_qiwa/ksa_qiwa/doctype/qiwa_contract/test_qiwa_contract.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2026, Salman Adayatt and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestQIWAContract(FrappeTestCase):
-	pass
